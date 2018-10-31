2dcc0a312a27f02dc4e284a79905300ec6646945
Darren Carlin <dazftw@gmail.com>
2018-10-31 16:56:25 -0400

    basic functionality in place

--- index.js
+++ index.js
@@ -1,9 +1,83 @@
-document.getElementById("convert").addEventListener("click", evt => {
-  evt.preventDefault();
-  let text = document.getElementById("text").value;
-  newText = text.replace("i hurt myself", "i did a hurt");
-  //let arr = text.split(/,?\s+/);
-  document.getElementById("output").innerHTML = newText;
+const words = {
+  help: "halp",
+  me: "me",
+  attention: "attenchon",
+  stop: "staph",
+  that: "dat",
+  thats: "dats",
+  this: "dis",
+  fuck: "furk",
+  fuckery: "furkery",
+  brother: "brofur",
+  brothers: "brofurs",
+  sister: "sisfur",
+  sisters: "sisfurs",
+  parent: "pawrent",
+  parents: "pawrents",
+  paws: "stumps",
+  with: "wid",
+  please: "peez",
+  look: "lewk",
+  give: "gib",
+  teeth: "teef",
+  think: "tink",
+  you: "u",
+  need: "ned",
+  to: "2",
+  its: "is",
+  human: "hooman"
+};
+
+const submitBtn = document.getElementById("submit");
+const textArea = document.getElementById("textarea");
+const clearBtn = document.getElementById("clear");
+const hashtagBtn = document.getElementById("convertToHash");
+
+textArea.addEventListener("keydown", event => {
+  if (event.keyCode === 13) {
+    convertText(event);
+  }
 });
 
-// two options to check, single words = array, sentences = complete string
+submitBtn.addEventListener("click", event => {
+  convertText(event);
+});
+
+hashtagBtn.addEventListener("click", event => {
+  convertHashtag(event);
+});
+
+let hashtagArr = [];
+
+function convertHashtag(event) {
+  event.preventDefault();
+  document.getElementById("outputform").innerHTML = "";
+  hashtagArr.forEach(element => {
+    document.getElementById("outputform").innerHTML += `#${element}`;
+  });
+}
+
+function convertText(event) {
+  event.preventDefault();
+
+  let text = document.getElementById("textarea").value.toLowerCase();
+  let arr = text.split(/,?\s+/);
+
+  arr.forEach(element => {
+    let output = words[element];
+    if (output === undefined) {
+      document.getElementById("outputform").innerHTML += "Word Not Found";
+    } else {
+      document.getElementById("outputform").innerHTML += `${output} `;
+      hashtagArr.push(output);
+    }
+  });
+}
+
+clearBtn.addEventListener("click", event => {
+  event.preventDefault();
+  document.getElementById("textarea").innerHTML = "";
+  document.getElementById("outputform").innerHTML = "";
+});
+
+// Allowed words (words that dont have a translation)
